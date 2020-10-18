2b5a7e1f21637cafd1a9d2bf5337ad4689a88e01
Theodore Anderson <theoanderson3@gmail.com>
2020-10-18 11:04:43 -0700

    clean up

--- index.js
+++ index.js
@@ -56,7 +56,10 @@ const wordReferenceToCsvFromString = (data = '') => {
 const wordReferenceToCsv = ({
   inputFile = INPUT_FILE,
   outputFile = OUTPUT_FILE,
-  noResultsFile = NO_RESULTS_FILE
+  noResultsFile = NO_RESULTS_FILE,
+  inputSourceLanguage,
+  outputTargetLanguage,
+  inputData = ''
 }) => {
   if(!fs.existsSync(inputFile)) {
     console.error('Input file path doesn\'t exist.');
@@ -70,6 +73,50 @@ const wordReferenceToCsv = ({
 
   if(!fs.existsSync(noResultsFile)) {
     console.error('No results file path doesn\'t exist.');
+    return;
+  }
+
+  let sourceLanguage;
+  let targetLanguage;
+  try {
+     sourceLanguage = formatProcessArgv(process.argv[2]) || inputSourceLanguage;
+     targetLanguage = formatProcessArgv(process.argv[3]) || outputTargetLanguage;
+  }
+  catch(err) {
+    throw new Error('Must enter source and target language')
+  }
+  if (err) throw err;
+
+  if(inputFormat === 'string') {
+      if (inputData.length === 0)
+        throw new Error("Must have at least one line in input.csv!");
+      const lines = WordReferenceUtils.formatLines(inputData);
+      const batches = WordReferenceUtils.createBatches(lines);
+      let currentBatch = 0;
+      batches[0].forEach(line => WordReferenceUtils.fetchData({ line, sourceLanguage: sourceLanguage.value, targetLanguage: targetLanguage.value }));
+      currentBatch += 1;
+  
+      if (
+        WordReferenceUtils.determineIfFinishedProcessing(currentBatch, batches)
+      ) {
+        return;
+      } else {
+        let interval;
+  
+        interval = setInterval(() => {
+          batches[currentBatch].forEach(line => WordReferenceUtils.fetchData({ line, sourceLanguage: sourceLanguage.value, targetLanguage: targetLanguage.value }));
+          if (
+            WordReferenceUtils.determineIfFinishedProcessing(
+              currentBatch,
+              batches
+            )
+          ) {
+            clearInterval(interval);
+            return;
+          }
+          currentBatch += 1;
+        }, DEBOUNCE_DURATION);
+      }
     return;
   }
 
