d8f4c7cf891e22ab39e3a5ae6783e9aca85bad61
Theodore Anderson <theoanderson3@gmail.com>
2020-10-18 10:50:12 -0700

    add method for from string

--- index.js
+++ index.js
@@ -11,6 +11,48 @@ const formatProcessArgv = argv => {
 }
 
 
+const wordReferenceToCsvFromString = (data = '') => {
+    let sourceLanguage;
+    let targetLanguage;
+    try {
+       sourceLanguage = formatProcessArgv(process.argv[2]);
+       targetLanguage = formatProcessArgv(process.argv[3]);
+    }
+    catch(err) {
+      throw new Error('Must enter source and target language')
+    }
+    if (err) throw err;
+    if (data.length === 0)
+      throw new Error("Must have at least one line in input.csv!");
+    const lines = WordReferenceUtils.formatLines(data);
+    const batches = WordReferenceUtils.createBatches(lines);
+    let currentBatch = 0;
+    batches[0].forEach(line => WordReferenceUtils.fetchData({ line, sourceLanguage: sourceLanguage.value, targetLanguage: targetLanguage.value }));
+    currentBatch += 1;
+
+    if (
+      WordReferenceUtils.determineIfFinishedProcessing(currentBatch, batches)
+    ) {
+      return;
+    } else {
+      let interval;
+
+      interval = setInterval(() => {
+        batches[currentBatch].forEach(line => WordReferenceUtils.fetchData({ line, sourceLanguage: sourceLanguage.value, targetLanguage: targetLanguage.value }));
+        if (
+          WordReferenceUtils.determineIfFinishedProcessing(
+            currentBatch,
+            batches
+          )
+        ) {
+          clearInterval(interval);
+          return;
+        }
+        currentBatch += 1;
+      }, DEBOUNCE_DURATION);
+    }
+};
+
 const wordReferenceToCsv = () => {
 
   fs.readFile(INPUT_FILE, FORMATS.UTF8, (err, data) => {
@@ -57,5 +99,6 @@ const wordReferenceToCsv = () => {
 };
 
 module.exports = {
-  wordReferenceToCsv
+  wordReferenceToCsv,
+  wordReferenceToCsvFromString
 };
